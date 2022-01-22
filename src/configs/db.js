e9e1372cd59648b8ec1c4b9fef76cdfd9deb85f6
Lakshmi Kavya <vankadaralakshmikavya@gmail.com>
2022-01-22 18:53:56 +0530

    7th  commit

--- src/configs/db.js
+++ src/configs/db.js
@@ -4,13 +4,16 @@ const mongoose = require('mongoose');
 //     return mongoose.connect("mongodb+srv://kavya_047:kavya_047@cluster0.vrril.mongodb.net/Purplle_Database")
 // }
 
+const mongoose = require("mongoose");
+require("dotenv").config();
+
 module.exports = () => {
-    return mongoose.connect(
-      process.env.MONGODB_URI ||
-        `mongodb+srv://kavya_047:kavya_047@cluster0.vrril.mongodb.net/Purplle_Database`,
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      }
-    );
-  };
+  return mongoose.connect(
+    process.env.MONGODB_URI ||
+      `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zkdfr.mongodb.net/bellavitaData?retryWrites=true&w=majority`,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  );
+};
